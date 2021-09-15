38e1a004f593c6c37660cb2b6980e0db52582c34
koh0012 <77301335+koh0012@users.noreply.github.com>
2021-09-15 18:33:56 -0500

    Feature 1 Added

--- Features.java
+++ Features.java
@@ -1,9 +1,35 @@
 package a1;
 
+import java.util.Scanner;
+
 public class Features {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter 0 for Int-To-String ");
+		int i = in.nextInt();
+		if (i == 0){
+			System.out.print("Welcome to Int-To-String. Please enter any value from 0 to 2: ");
+			int j = in.nextInt();
+			if (j == 0) {
+				System.out.println("John");
+			}
+			
+			else if (j == 1) {
+				System.out.println("Ron");
+			}
+			
+			else if (j == 2) {
+				System.out.println("Joe");
+			}
+			
+			else {
+				System.out.println("Invalid");
+			}
+		}
+		
+		in.close();
 
 	}
 
