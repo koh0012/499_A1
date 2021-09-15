05833d3e9ad59c7971b4df5b32d57a06618c7b61
koh0012 <77301335+koh0012@users.noreply.github.com>
2021-09-15 18:43:36 -0500

    Add files via upload

--- Features.java
+++ Features.java
@@ -7,7 +7,7 @@ public class Features {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter 0 for Int-To-String ");
+		System.out.print("Enter 0 for Int-To-String or Enter 1 for String-To-Int or any other number to exit. ");
 		int i = in.nextInt();
 		if (i == 0){
 				
@@ -30,7 +30,28 @@ public class Features {
 			}
 			
 			
-		} else {
+		}
+		else if (i == 1) {
+			System.out.print("Welcome to String-To-Int. Please enter either John, Ron, or Joe ");
+			String s = in.next();
+			if (s.equals("John")) {
+				System.out.println("0");
+			}
+			
+			else if (s.equals("Ron")) {
+				System.out.println("1");
+			}
+			
+			else if (s.equals("Joe")) {
+				System.out.println("2");
+			}
+			
+			else {
+				System.out.println("Invalid");
+			}
+			
+		}
+		else {
 			System.out.println("Program Terminated.");
 		}
 		
